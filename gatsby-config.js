0466b09f54d39f00a1e7579e79518dddce9f99ad
Kirill <koshkin.kirilll@gmail.com>
2021-06-09 12:50:04 +0300

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,27 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+plugins: [
+  {
+    resolve: 'gatsby-plugin-favicons',
+    options: {
+      logo: './src/favicon.ico',
+      appName: 'My Website',
+      background: '#fff',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        yandex: false,
+        windows: false
+      }
+    }
+  }
+]
 
 module.exports = {
   plugins: [`gatsby-plugin-react-helmet`]
 }
+
